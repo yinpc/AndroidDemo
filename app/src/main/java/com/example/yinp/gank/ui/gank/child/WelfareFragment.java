b7f37038f753f44f25df3b353ec4e330d90b5fc0
yinpc <yinpengrar@gmail.com>
2018-04-15 20:14:57 +0800

    201804152014

--- app/src/main/java/com/example/yinp/gank/ui/gank/child/WelfareFragment.java
+++ app/src/main/java/com/example/yinp/gank/ui/gank/child/WelfareFragment.java
@@ -5,8 +5,10 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.OrientationHelper;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +16,8 @@ import android.view.ViewGroup;
 import com.example.yinp.gank.R;
 import com.example.yinp.gank.databinding.FragmentWelfareBinding;
 import com.example.yinp.gank.view.statusbar.MyRecyclerViewAdapter;
+
+import java.util.ArrayList;
 
 public class WelfareFragment extends Fragment {
 
@@ -30,7 +34,20 @@ public class WelfareFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        bindingView.recyclerView.setLayoutManager(new LinearLayoutManager(this.getActivity()));
-        bindingView.recyclerView.setAdapter(new MyRecyclerViewAdapter(this.getActivity()));
+//        bindingView.recyclerView.setLayoutManager(new LinearLayoutManager(this.getActivity()));
+//        bindingView.recyclerView.setLayoutManager(new GridLayoutManager(this.getActivity(),2));
+        StaggeredGridLayoutManager layoutManager = new StaggeredGridLayoutManager(2, OrientationHelper.VERTICAL);
+        layoutManager.setGapStrategy(StaggeredGridLayoutManager.GAP_HANDLING_NONE);
+        bindingView.recyclerView.setLayoutManager(layoutManager);
+        bindingView.recyclerView.setAdapter(new MyRecyclerViewAdapter(this.getActivity(), getData()));
+    }
+
+    private ArrayList<String> getData() {
+        ArrayList<String> data = new ArrayList<>();
+        String temp = " item";
+        for (int i = 1; i <= 100; i++) {
+            data.add(i + temp);
+        }
+        return data;
     }
 }
--- app/src/main/java/com/example/yinp/gank/view/statusbar/MyRecyclerViewAdapter.java
+++ app/src/main/java/com/example/yinp/gank/view/statusbar/MyRecyclerViewAdapter.java
@@ -1,23 +1,30 @@
 package com.example.yinp.gank.view.statusbar;
 
 import android.content.Context;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.yinp.gank.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAdapter.ViewHolder> {
     private final LayoutInflater mLayoutInflater;
     private final Context mContext;
-    private String[] mTitles;
+    private ArrayList<String> mTitles;
 
-    public MyRecyclerViewAdapter(Context context) {
-        mTitles = context.getResources().getStringArray(R.array.titles);
+    public MyRecyclerViewAdapter(Context context, ArrayList<String> titles) {
+//        mTitles = context.getResources().getStringArray(R.array.titles);
+        mTitles = titles;
         mContext = context;
         mLayoutInflater = LayoutInflater.from(mContext);
     }
@@ -28,28 +35,42 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
     }
 
     @Override
-    public void onBindViewHolder(MyRecyclerViewAdapter.ViewHolder holder, int position) {
-        holder.mTextView.setText(mTitles[position]);
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.mTextView.setText(mTitles.get(position));
+        int normalMargin = dp2px(mContext, 8);
+        int diffMargin = dp2px(mContext, 100);
+        if (position == 0) {
+            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(holder.mCardView.getLayoutParams());
+            lp.setMargins(normalMargin, diffMargin, normalMargin, normalMargin);
+            holder.mCardView.setLayoutParams(lp);
+        }
     }
 
     @Override
     public int getItemCount() {
-        return mTitles == null ? 0 : mTitles.length;
+        return mTitles == null ? 0 : mTitles.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        CardView mCardView;
         TextView mTextView;
 
         public ViewHolder(View itemView) {
             super(itemView);
+            mCardView = itemView.findViewById(R.id.card_view);
             mTextView = itemView.findViewById(R.id.text_view);
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Log.d("NormalTextViewHolder", "onClick--> position = " + getLayoutPosition());
+//                    Log.d("NormalTextViewHolder", "onClick--> position = " + getLayoutPosition());
                     Toast.makeText(mContext, "click", Toast.LENGTH_LONG).show();
                 }
             });
         }
+    }
+
+    public static int dp2px(Context context, float dpVal) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                dpVal, context.getResources().getDisplayMetrics());
     }
 }
