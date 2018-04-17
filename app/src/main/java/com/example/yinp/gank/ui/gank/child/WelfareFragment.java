5bdb3b9b7f6775d0ba15fc1a4d450aa82519f90c
yinpc <yinpengrar@gmail.com>
2018-04-17 20:55:03 +0800

    simple loadmore

--- app/src/main/java/com/example/yinp/gank/ui/gank/child/WelfareFragment.java
+++ app/src/main/java/com/example/yinp/gank/ui/gank/child/WelfareFragment.java
@@ -6,7 +6,9 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.OrientationHelper;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -32,6 +34,7 @@ public class WelfareFragment extends Fragment {
     private FragmentWelfareBinding bindingView;
     private WelfareRecyclerViewAdapter adapter;
     private ArrayList<FeedImageInfo> feedImageInfos;
+    private int page;
 
     @Nullable
     @Override
@@ -57,8 +60,48 @@ public class WelfareFragment extends Fragment {
         adapter = new WelfareRecyclerViewAdapter(getContext(), feedImageInfos);
         bindingView.recyclerView.setAdapter(adapter);
 
-        loadData(true);
+        page = 1;
+        loadData(true, page);
         initRefreshView();
+        initLoadMoreListener();
+    }
+
+    private void initLoadMoreListener() {
+        bindingView.recyclerView.setOnScrollListener(new RecyclerView.OnScrollListener() {
+            int lastVisibleItem;
+
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                if (adapter != null) {
+                    if (newState == RecyclerView.SCROLL_STATE_IDLE
+                            && lastVisibleItem + 1 == adapter.getItemCount()) {
+                        bindingView.srlWelfare.setRefreshing(true);
+                        page++;
+                        loadData(false, page);
+                    }
+                }
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                View lastChildView = recyclerView.getLayoutManager().getChildAt(recyclerView.getLayoutManager().getChildCount()-1);
+                //得到lastChildView的bottom坐标值
+//                int lastChildBottom = lastChildView.getBottom();
+                //得到Recyclerview的底部坐标减去底部padding值，也就是显示内容最底部的坐标
+//                int recyclerBottom =  recyclerView.getBottom()-recyclerView.getPaddingBottom();
+                //通过这个lastChildView得到这个view当前的position值
+                lastVisibleItem = recyclerView.getLayoutManager().getPosition(lastChildView);
+
+                //判断lastChildView的bottom值跟recyclerBottom
+                //判断lastPosition是不是最后一个position
+                //如果两个条件都满足则说明是真正的滑动到了底部
+//                if(lastChildBottom == recyclerBottom && lastPosition == recyclerView.getLayoutManager().getItemCount()-1 ){
+//                    Toast.makeText(RecyclerActivity.this, "滑动到底了", Toast.LENGTH_SHORT).show();
+//                }
+            }
+        });
     }
 
     private void initRefreshView() {
@@ -69,20 +112,21 @@ public class WelfareFragment extends Fragment {
                 bindingView.srlWelfare.postDelayed(new Runnable() {
                     @Override
                     public void run() {
-                        loadData(true);
+                        page = 1;
+                        loadData(true, page);
                     }
                 }, 100);
             }
         });
     }
 
-    private void loadData(final boolean isRefresh){
+    private void loadData(final boolean isRefresh, int page) {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://gank.io/api/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         HttpClient client = retrofit.create(HttpClient.class);
-        Call<GankIoDataBean> call = client.getGankIoData("福利", 1, 20);
+        Call<GankIoDataBean> call = client.getGankIoData("福利", page, 20);
         call.enqueue(new Callback<GankIoDataBean>() {
             @Override
             public void onResponse(Call<GankIoDataBean> call, Response<GankIoDataBean> response) {
@@ -92,12 +136,12 @@ public class WelfareFragment extends Fragment {
 //                        Toast.LENGTH_LONG)
 //                        .show();
 //                ArrayList<String> mUrls = new ArrayList<>();
-                if (isRefresh){
+                if (isRefresh) {
                     feedImageInfos.clear();
                 }
                 adapter.notifyDataSetChanged();
 
-                for(GankIoDataBean.ResultBean s : response.body().getResults()){
+                for (GankIoDataBean.ResultBean s : response.body().getResults()) {
 //                    mUrls.add(s.getUrl());
                     feedImageInfos.add(new FeedImageInfo(s.getUrl()));
                 }
