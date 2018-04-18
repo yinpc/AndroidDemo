216a7a4e5ff60f44f9098244f486ab9a9894563f
yinpc <yinpengrar@gmail.com>
2018-04-18 17:59:28 +0800

    xrecyclerview

--- app/src/main/java/com/example/yinp/gank/adapter/WelfareRecyclerViewAdapter.java
+++ app/src/main/java/com/example/yinp/gank/adapter/WelfareRecyclerViewAdapter.java
@@ -1,23 +1,16 @@
 package com.example.yinp.gank.adapter;
 
 import android.content.Context;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.support.v7.widget.RecyclerView;
-import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.RequestOptions;
-import com.bumptech.glide.request.target.BitmapImageViewTarget;
 import com.example.yinp.gank.R;
 import com.example.yinp.gank.bean.FeedImageInfo;
-import com.example.yinp.gank.view.DriverViewTarget;
 
 import java.util.ArrayList;
 
@@ -40,26 +33,23 @@ public class WelfareRecyclerViewAdapter extends RecyclerView.Adapter<WelfareRecy
 
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
-            String url = mFeedImageInfos.get(position).getUrl();
+//        String url = mFeedImageInfos.get(position).getUrl();
 
-            RequestOptions options = new RequestOptions()
-                    .placeholder(new ColorDrawable(Color.BLACK))
-                    .override(holder.mImageView.getWidth(), BitmapImageViewTarget.SIZE_ORIGINAL)
-                    .fitCenter();
+        // dynamic height
+//            RequestOptions options = new RequestOptions()
+//                    .placeholder(new ColorDrawable(Color.BLACK))
+//                    .override(holder.mImageView.getWidth(), BitmapImageViewTarget.SIZE_ORIGINAL)
+//                    .fitCenter();
+//
+//            Glide.with(mContext)
+//                    .asBitmap()
+//                    .apply(options)
+//                    .load(url)
+//                    .into(new DriverViewTarget(holder.mImageView, mFeedImageInfos.get(position)));
 
-            Glide.with(mContext)
-                    .asBitmap()
-                    .apply(options)
-                    .load(url)
-                    .into(new DriverViewTarget(holder.mImageView, mFeedImageInfos.get(position)));
-    }
-
-    public static int getScreenWidth(Context context) {
-        WindowManager wm = (WindowManager) context
-                .getSystemService(Context.WINDOW_SERVICE);
-        DisplayMetrics outMetrics = new DisplayMetrics();
-        wm.getDefaultDisplay().getMetrics(outMetrics);
-        return outMetrics.widthPixels;
+        Glide.with(mContext)
+                .load(mFeedImageInfos.get(position).getUrl())
+                .into(holder.mImageView);
     }
 
     @Override
@@ -76,7 +66,6 @@ public class WelfareRecyclerViewAdapter extends RecyclerView.Adapter<WelfareRecy
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-//                    Log.d("NormalTextViewHolder", "onClick--> position = " + getLayoutPosition());
                     Toast.makeText(mContext, "click", Toast.LENGTH_LONG).show();
                 }
             });
--- app/src/main/java/com/example/yinp/gank/ui/gank/child/WelfareFragment.java
+++ app/src/main/java/com/example/yinp/gank/ui/gank/child/WelfareFragment.java
@@ -20,6 +20,7 @@ import com.example.yinp.gank.bean.GankIoDataBean;
 import com.example.yinp.gank.databinding.FragmentWelfareBinding;
 import com.example.yinp.gank.http.HttpClient;
 import com.example.yinp.gank.adapter.WelfareRecyclerViewAdapter;
+import com.example.yinp.gank.view.recyclerview.XRecyclerView;
 
 import java.util.ArrayList;
 
@@ -47,64 +48,26 @@ public class WelfareFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-//        bindingView.recyclerView.setLayoutManager(new LinearLayoutManager(this.getActivity()));
-//        bindingView.recyclerView.setLayoutManager(new GridLayoutManager(this.getActivity(),2));
-//        Gson gson = new GsonBuilder()
-//                .setDateFormat("yyyy-MM-dd")
-//                .create();
+        initView();
+        page = 1;
+        loadData(true, page);
+    }
 
+    private void initView() {
         StaggeredGridLayoutManager layoutManager = new StaggeredGridLayoutManager(2, OrientationHelper.VERTICAL);
         layoutManager.setGapStrategy(StaggeredGridLayoutManager.GAP_HANDLING_NONE);
         bindingView.recyclerView.setLayoutManager(layoutManager);
         feedImageInfos = new ArrayList<>();
         adapter = new WelfareRecyclerViewAdapter(getContext(), feedImageInfos);
         bindingView.recyclerView.setAdapter(adapter);
-
-        page = 1;
-        loadData(true, page);
-        initRefreshView();
-        initLoadMoreListener();
-    }
-
-    private void initLoadMoreListener() {
-        bindingView.recyclerView.setOnScrollListener(new RecyclerView.OnScrollListener() {
-            int lastVisibleItem;
-
+        bindingView.recyclerView.setLoadingListener(new XRecyclerView.LoadingListener() {
             @Override
-            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
-                super.onScrollStateChanged(recyclerView, newState);
-                if (adapter != null) {
-                    if (newState == RecyclerView.SCROLL_STATE_IDLE
-                            && lastVisibleItem + 1 == adapter.getItemCount()) {
-                        bindingView.srlWelfare.setRefreshing(true);
-                        page++;
-                        loadData(false, page);
-                    }
-                }
-            }
-
-            @Override
-            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-                super.onScrolled(recyclerView, dx, dy);
-                View lastChildView = recyclerView.getLayoutManager().getChildAt(recyclerView.getLayoutManager().getChildCount()-1);
-                //得到lastChildView的bottom坐标值
-//                int lastChildBottom = lastChildView.getBottom();
-                //得到Recyclerview的底部坐标减去底部padding值，也就是显示内容最底部的坐标
-//                int recyclerBottom =  recyclerView.getBottom()-recyclerView.getPaddingBottom();
-                //通过这个lastChildView得到这个view当前的position值
-                lastVisibleItem = recyclerView.getLayoutManager().getPosition(lastChildView);
-
-                //判断lastChildView的bottom值跟recyclerBottom
-                //判断lastPosition是不是最后一个position
-                //如果两个条件都满足则说明是真正的滑动到了底部
-//                if(lastChildBottom == recyclerBottom && lastPosition == recyclerView.getLayoutManager().getItemCount()-1 ){
-//                    Toast.makeText(RecyclerActivity.this, "滑动到底了", Toast.LENGTH_SHORT).show();
-//                }
+            public void onLoadMore() {
+                page++;
+                loadData(false, page);
             }
         });
-    }
 
-    private void initRefreshView() {
         bindingView.srlWelfare.setColorSchemeColors(getResources().getColor(R.color.colorTheme));
         bindingView.srlWelfare.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
@@ -130,22 +93,15 @@ public class WelfareFragment extends Fragment {
         call.enqueue(new Callback<GankIoDataBean>() {
             @Override
             public void onResponse(Call<GankIoDataBean> call, Response<GankIoDataBean> response) {
-//                System.out.print(response.body().toString());
-//                Toast.makeText(getContext(),
-//                        response.body().getResults().get(0).getUrl(),
-//                        Toast.LENGTH_LONG)
-//                        .show();
-//                ArrayList<String> mUrls = new ArrayList<>();
                 if (isRefresh) {
                     feedImageInfos.clear();
                 }
-                adapter.notifyDataSetChanged();
 
                 for (GankIoDataBean.ResultBean s : response.body().getResults()) {
-//                    mUrls.add(s.getUrl());
                     feedImageInfos.add(new FeedImageInfo(s.getUrl()));
                 }
 
+                bindingView.recyclerView.loadMoreComplete();
                 adapter.notifyDataSetChanged();
                 bindingView.srlWelfare.setRefreshing(false);
             }
